896b9f0a0cf39245b9cdbe54456d4f41131d0177
mekroket <67954467+mekroket@users.noreply.github.com>
2020-10-26 20:55:42 +0300

    fonksiyonlar ve class kullanımıyla ilgili bir ev kontrol etme projesi

---
+++ Yazılım Pyhton/Ders denemelerim/atm_yapma.py
@@ -0,0 +1,64 @@
+import time
+print("""
+**************************************************
+        ATM MAKİNESİNE HOŞGELDİNİZ
+        İŞLEMLERİMİZ:
+    1-PARA EKLE
+    2-PARA ÇEK
+    4-ÇIKIŞ YAPMAK İÇİN Q YA BASINIZ
+*************************************************
+""")
+
+para=2500
+
+sys_kullanıcı="oguz"
+sys_şifre="1234"
+
+kullanıcı=input("KULLANICI ADNIZI GİRİNİZ: ")
+şifre=input("Şifrenizi giriniz: ")
+if(sys_kullanıcı==kullanıcı and sys_şifre==şifre):
+    time.sleep(3)
+    print("giriş yapıldı,sisteme yönlendiriliyorsunuz........")
+    işlem1=input("Yapmak istediğiniz işlemi seçiniz: ")
+    if(işlem1=="1"):
+        ekle=int(input("ne kafar para eklemek istiyorsunuz: "))
+        para+=ekle
+        print("En son para durumunuz: ",para)
+    elif (işlem1 == "2"):
+        ekle = int(nput("ne kafar para çıkarmak istiyorsunuz: "))
+        para -= ekle
+        print("En son para durumunuz: ", para)
+    else:
+        print("çıkış yapılıyor.....")
+        exit()
+
+elif(sys_kullanıcı==kullanıcı and sys_şifre!=şifre):
+    time.sleep(3)
+    print("şifreniz hatalı")
+elif(sys_kullanıcı!=kullanıcı and sys_şifre==şifre):
+    time.sleep(3)
+    print("kullanıcı adınız hatalı")
+else:
+    print("iki durumda hatalı")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
